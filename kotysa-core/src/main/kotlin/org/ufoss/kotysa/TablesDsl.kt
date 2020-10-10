57eb6d2e7bd46ad9f96d776b3713590623a89bda
pull-vert <5107035+pull-vert@users.noreply.github.com>
2020-10-10 10:58:02 +0200

    code style

--- kotysa-core/src/main/kotlin/org/ufoss/kotysa/TablesDsl.kt
+++ kotysa-core/src/main/kotlin/org/ufoss/kotysa/TablesDsl.kt
@@ -8,7 +8,9 @@ import kotlin.reflect.KClass
 
 
 @KotysaMarker
-public abstract class TablesDsl<T : TablesDsl<T, U>, U : TableDsl<*, *>> protected constructor(private val init: T.() -> Unit) {
+public abstract class TablesDsl<T : TablesDsl<T, U>, U : TableDsl<*, *>> protected constructor(
+        private val init: T.() -> Unit
+) {
 
     private val allTables = mutableMapOf<KClass<*>, Table<*>>()
     private val allColumns = mutableMapOf<(Any) -> Any?, Column<*, *>>()
