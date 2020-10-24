a5e65e5748f5a0997cd5c3c7910aabe3aa02c86b
pull-vert <5107035+pull-vert@users.noreply.github.com>
2020-10-24 14:36:32 +0200

    prepare MySQL tests with r2dbc

---
+++ kotysa-spring-r2dbc/src/test/kotlin/org/ufoss/kotysa/r2dbc/mysql/AbstractR2dbcMysqlTest.kt
@@ -0,0 +1,63 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package org.ufoss.kotysa.r2dbc.mysql
+
+import org.junit.jupiter.api.AfterAll
+import org.springframework.beans.factory.getBean
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.fu.kofu.application
+import org.springframework.fu.kofu.r2dbc.r2dbc
+import org.springframework.r2dbc.core.DatabaseClient
+import org.testcontainers.containers.MySQLContainer
+import org.ufoss.kotysa.r2dbc.coSqlClient
+import org.ufoss.kotysa.r2dbc.sqlClient
+import org.ufoss.kotysa.test.Repository
+import org.ufoss.kotysa.test.mysqlTables
+
+class KMySQLContainer : MySQLContainer<KMySQLContainer>()
+
+
+abstract class AbstractR2dbcMysqlTest<T : Repository> {
+
+    protected abstract val repository: T
+
+    protected inline fun <reified U : Repository> startContext(): ConfigurableApplicationContext {
+        // PostgreSQL testcontainers must be started first to get random Docker mapped port
+        val mysqlContainer = KMySQLContainer()
+                .withDatabaseName("mysql")
+                .withUsername("mysql")
+                .withPassword("test")
+        mysqlContainer.start()
+
+        return application {
+            beans {
+                bean { mysqlContainer }
+                bean<U>()
+                bean { ref<DatabaseClient>().sqlClient(mysqlTables) }
+                bean { ref<DatabaseClient>().coSqlClient(mysqlTables) }
+            }
+            listener<ApplicationReadyEvent> {
+                ref<U>().init()
+            }
+            r2dbc {
+                url = "r2dbc:mysql://${mysqlContainer.containerIpAddress}:${mysqlContainer.firstMappedPort}/db"
+                username = "postgres"
+                transactional = true
+            }
+        }.run()
+    }
+
+    protected abstract val context: ConfigurableApplicationContext
+
+    protected inline fun <reified U : Repository> getContextRepository() = context.getBean<U>()
+
+    @AfterAll
+    fun afterAll() {
+        repository.delete()
+        context.getBean<MySQLContainer<*>>().stop()
+        context.close()
+    }
+}
