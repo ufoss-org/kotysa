f5c64f80800d13fae8de3dfffbbf48c84edd9aff
pull-vert <5107035+pull-vert@users.noreply.github.com>
2020-10-27 05:16:42 +0100

    add R2DbcJavaEntityMysqlTest

---
+++ kotysa-spring-r2dbc/src/test/kotlin/org/ufoss/kotysa/r2dbc/mysql/R2DbcJavaEntityMysqlTest.kt
@@ -0,0 +1,53 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package org.ufoss.kotysa.r2dbc.mysql
+
+import org.springframework.r2dbc.core.DatabaseClient
+import org.ufoss.kotysa.r2dbc.JavaUserRepository
+import org.ufoss.kotysa.r2dbc.SpringR2dbcJavaEntityTest
+import org.ufoss.kotysa.tables
+import org.ufoss.kotysa.test.JavaUser
+
+
+class R2DbcJavaEntityMysqlTest :
+        AbstractR2dbcMysqlTest<JavaUserMysqlRepository>(), SpringR2dbcJavaEntityTest<JavaUserMysqlRepository> {
+    override val context = startContext<JavaUserMysqlRepository>()
+    override val repository = getContextRepository<JavaUserMysqlRepository>()
+}
+
+private val tables =
+        tables().mysql {
+            table<JavaUser> {
+                name = "java_users"
+                column { it[JavaUser::getLogin].varchar {
+                    size = 255
+                } }.primaryKey()
+                column {
+                    it[JavaUser::getFirstname].varchar {
+                        name = "fname"
+                        size = 255
+                    }
+                }
+                column {
+                    it[JavaUser::getLastname].varchar {
+                        name = "lname"
+                        size = 255
+                    }
+                }
+                column { it[JavaUser::isAdmin].boolean() }
+                column { it[JavaUser::getAlias1].varchar {
+                    size = 255
+                } }
+                column { it[JavaUser::getAlias2].varchar {
+                    size = 255
+                } }
+                column { it[JavaUser::getAlias3].varchar {
+                    size = 255
+                } }
+            }
+        }
+
+
+class JavaUserMysqlRepository(client: DatabaseClient) : JavaUserRepository(client, tables)
