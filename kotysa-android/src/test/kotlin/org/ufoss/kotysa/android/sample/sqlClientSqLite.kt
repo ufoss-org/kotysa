5a71ad8834fede7ee86573b49d982c434312689b
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-01-17 10:39:06 +0100

    WIP53 : uncomment sample for SqLite

--- kotysa-android/src/test/kotlin/org/ufoss/kotysa/android/sample/sqlClientSqLite.kt
+++ kotysa-android/src/test/kotlin/org/ufoss/kotysa/android/sample/sqlClientSqLite.kt
@@ -3,92 +3,92 @@
  */
 
 package org.ufoss.kotysa.android.sample
-/*
+
 import android.database.sqlite.SQLiteOpenHelper
 import org.ufoss.kotysa.android.sqlClient
+import org.ufoss.kotysa.sqlite.SqLiteTable
+import org.ufoss.kotysa.tables
 
 @Suppress("UNUSED_VARIABLE")
 class UserRepositorySqLite(sqLiteOpenHelper: SQLiteOpenHelper) {
 
     data class Role(
-        val label: String,
-        val id: String
+            val label: String,
+            val id: String
     )
 
     data class User(
-        val firstname: String,
-        val lastname: String,
-        val isAdmin: Boolean,
-        val roleId: String,
-        val alias: String? = null,
-        val id: String
+            val firstname: String,
+            val lastname: String,
+            val isAdmin: Boolean,
+            val roleId: String,
+            val alias: String? = null,
+            val id: String
     )
 
-    val tables =
-        org.ufoss.kotysa.tables().sqlite {
-            // choose database type
-            table<Role> {
-                name = "roles"
-                column { it[Role::id].text() }
-                    .primaryKey()
-                column { it[Role::label].text() }
-            }
-            table<User> {
-                name = "users"
-                column { it[User::id].text() }
-                    .primaryKey()
-                column { it[User::firstname].text {
-                    name = "fname"
-                } }
-                column { it[User::lastname].text {
-                    name = "lname"
-                } }
-                column { it[User::isAdmin].integer() }
-                column { it[User::roleId].text() }
-                    .foreignKey<Role>()
-                column { it[User::alias].text() }
-            }
-        }
+    object SQLITE_ROLE : SqLiteTable<Role>("roles") {
+        val id = text(Role::id).primaryKey()
+        val label = text(Role::label)
+    }
 
-    val roleUser = Role("user", "ghi")
-    val roleAdmin = Role("admin", "jkl")
+    object SQLITE_USER : SqLiteTable<User>("users") {
+        val id = text(User::id).primaryKey()
+        val firstname = text(User::firstname, "fname")
+        val lastname = text(User::lastname, "lname")
+        val isAdmin = integer(User::isAdmin)
+        val roleId = text(User::roleId)
+                .foreignKey(SQLITE_ROLE.id, "FK_users_roles")
+        val alias = text(User::alias)
+    }
 
-    val userJdoe = User("John", "Doe", false, roleUser.id, id = "abc")
-    val userBboss = User("Big", "Boss", true, roleAdmin.id, "TheBoss", "def")
+    val tables = tables().sqlite(
+            SQLITE_ROLE,
+            SQLITE_USER,
+    )
+
+    private val roleUser = Role("user", "ghi")
+    private val roleAdmin = Role("admin", "jkl")
+
+    private val userJdoe = User("John", "Doe", false, roleUser.id, id = "abc")
+    private val userBboss = User("Big", "Boss", true, roleAdmin.id, "TheBoss", "def")
 
     private class UserWithRoleDto(
-        val lastname: String,
-        val role: String
+            val lastname: String,
+            val role: String
     )
 
     private val sqlClient = sqLiteOpenHelper.sqlClient(tables)
 
-    fun simplifiedExample() = sqlClient.run {
-        createTable<User>()
-        deleteAllFromTable<User>()
-        insert(userJdoe, userBboss)
+    fun simplifiedExample() {
+        sqlClient createTable SQLITE_ROLE
+        sqlClient deleteAllFrom SQLITE_ROLE
+        sqlClient.insert(roleUser, roleAdmin)
 
-        val count = countAll<User>()
+        sqlClient createTable SQLITE_USER
+        sqlClient deleteAllFrom SQLITE_USER
+        sqlClient.insert(userJdoe, userBboss)
 
-        val all = selectAll<User>()
+        // val count = sqlClient countAll<User>()
 
-        val johny = select { UserWithRoleDto(it[User::lastname], it[Role::label]) }
-            .innerJoin<Role>().on { it[User::roleId] }
-            .where { it[User::alias] eq "Johny" }
-            // null String accepted        ^^^^^ , if alias=null, gives "WHERE user.alias IS NULL"
-            .or { it[User::alias] eq "Johnny" }
-            .fetchFirst()
+        val all = sqlClient selectAllFrom SQLITE_USER
 
-        val nbUpdated = updateTable<User>()
-            .set { it[User::lastname] = "NewLastName" }
-            .innerJoin<Role>().on { it[User::roleId] }
-            .where { it[Role::label] eq roleUser.label }
-            // null String forbidden      ^^^^^^^^^^^^
-            .execute()
+        val johny = (sqlClient select { UserWithRoleDto(it[SQLITE_USER.lastname], it[SQLITE_ROLE.label]) }
+                from SQLITE_USER innerJoin SQLITE_ROLE on SQLITE_USER.roleId eq SQLITE_ROLE.id
+                where SQLITE_USER.alias eq "Johny"
+                // null String accepted        ^^^^^ , if alias=null, gives "WHERE user.alias IS NULL"
+                or SQLITE_USER.alias eq "Johnny"
+                ).fetchFirst()
 
-        val nbDeleted = deleteFromTable<User>()
-            .innerJoin<Role>().on { it[User::roleId] }
-            .where { it[Role::label] eq roleUser.label }
-            .execute()
+        val nbUpdated = (sqlClient update SQLITE_USER
+                set SQLITE_USER.lastname eq "NewLastName"
+                innerJoin SQLITE_ROLE on SQLITE_USER.roleId eq SQLITE_ROLE.id
+                where SQLITE_ROLE.label eq roleUser.label
+                // null String forbidden      ^^^^^^^^^^^^
+                ).execute()
+
+        val nbDeleted = (sqlClient deleteFrom SQLITE_USER
+                innerJoin SQLITE_ROLE on SQLITE_USER.roleId eq SQLITE_ROLE.id
+                where SQLITE_ROLE.label eq roleUser.label
+                ).execute()
     }
-}*/
+}
