4f4bd3531b878062f1cad5e492a50bd6175d3fb5
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-05-02 10:48:44 +0200

    Add order by (#48)
    support order by

---
+++ kotysa-core/src/main/kotlin/org/ufoss/kotysa/Order.kt
@@ -0,0 +1,9 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package org.ufoss.kotysa
+
+internal enum class Order {
+    ASC, DESC
+}
