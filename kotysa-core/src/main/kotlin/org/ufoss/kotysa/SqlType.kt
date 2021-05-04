c75c0e417525c57450ef8bc29e0ce960490db8bb
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-05-04 23:33:45 +0200

    start long (bigint) support

--- kotysa-core/src/main/kotlin/org/ufoss/kotysa/SqlType.kt
+++ kotysa-core/src/main/kotlin/org/ufoss/kotysa/SqlType.kt
@@ -14,7 +14,9 @@ public enum class SqlType(internal val fullType: String) {
 
     // numbers
     INTEGER("INTEGER"),
+    BIGINT("BIGINT"),
     SERIAL("SERIAL"),
+    BIGSERIAL("BIGSERIAL"),
 
     // date
     TIMESTAMP("TIMESTAMP"),
