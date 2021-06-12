6fe174b9ab1afd9fdcb67e4601b25829b68bf950
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-06-12 19:08:39 +0200

    add mariadb sample

---
+++ kotysa-core/src/test/kotlin/org/ufoss/kotysa/sample/mariadbTables.kt
@@ -0,0 +1,27 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package org.ufoss.kotysa.sample
+
+import org.ufoss.kotysa.mariadb.MariadbTable
+import org.ufoss.kotysa.tables
+
+data class MariadblUser(
+        val firstname: String,
+        val lastname: String,
+        val isAdmin: Boolean,
+        val alias: String? = null,
+        val id: Int?
+)
+
+object MARIADB_USER : MariadbTable<MariadblUser>() {
+    val id = autoIncrementInteger(MariadblUser::id)
+            .primaryKey()
+    val firstname = varchar(MariadblUser::firstname, "fname")
+    val lastname = varchar(MariadblUser::lastname, "lname")
+    val isAdmin = boolean(MariadblUser::isAdmin)
+    val alias = varchar(MariadblUser::alias)
+}
+
+fun mariadbTables() = tables().mariadb(MARIADB_USER)
