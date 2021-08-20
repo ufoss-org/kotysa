5e34473897d35c6d191aed4d9d05cf948714d348
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-08-20 17:58:05 +0200

    start jdbc impl

---
+++ kotysa-jdbc/src/main/kotlin/org/ufoss/kotysa/jdbc/SqlClientDeleteJdbc.kt
@@ -0,0 +1,55 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package org.ufoss.kotysa.jdbc
+
+import org.ufoss.kotysa.*
+import java.sql.Connection
+
+internal class SqlClientDeleteJdbc private constructor() : DefaultSqlClientDeleteOrUpdate() {
+
+    internal class FirstDelete<T : Any> internal constructor(
+            override val connection: Connection,
+            override val tables: Tables,
+            override val table: Table<T>,
+    ) : FirstDeleteOrUpdate<T, SqlClientDeleteOrUpdate.DeleteOrUpdate<T>, T,
+            SqlClientDeleteOrUpdate.Where<T>>(DbAccessType.JDBC), SqlClientDeleteOrUpdate.FirstDeleteOrUpdate<T>,
+            Return<T> {
+        override val where = Where(connection, properties)
+        override val from: SqlClientDeleteOrUpdate.DeleteOrUpdate<T> by lazy {
+            Delete(connection, properties)
+        }
+    }
+
+    internal class Delete<T : Any>(
+            override val connection: Connection,
+            override val properties: Properties<T>,
+    ) : DeleteOrUpdate<T, SqlClientDeleteOrUpdate.DeleteOrUpdate<T>, Any, SqlClientDeleteOrUpdate.Where<Any>>(),
+            SqlClientDeleteOrUpdate.DeleteOrUpdate<T>, Return<T> {
+        @Suppress("UNCHECKED_CAST")
+        override val where = Where(connection, properties as Properties<Any>)
+        override val from = this
+    }
+
+    internal class Where<T : Any>(
+            override val connection: Connection,
+            override val properties: Properties<T>
+    ) : DefaultSqlClientDeleteOrUpdate.Where<T, SqlClientDeleteOrUpdate.Where<T>>(), SqlClientDeleteOrUpdate.Where<T>, Return<T> {
+        override val where = this
+    }
+
+    private interface Return<T : Any> : DefaultSqlClientDeleteOrUpdate.Return<T>, SqlClientDeleteOrUpdate.Return {
+        val connection: Connection
+
+        override fun execute() = with(properties) {
+            val statement = connection.prepareStatement(deleteFromTableSql())
+            // 1) add all values from where part
+            jdbcBindWhereParams(statement)
+            // 2) reset index
+            index = 0
+
+            statement.executeUpdate()
+        }
+    }
+}
---
+++ kotysa-jdbc/src/main/kotlin/org/ufoss/kotysa/jdbc/SqlClientJdbc.kt
@@ -0,0 +1,98 @@
+/*
+ * This is free and unencumbered software released into the public domain, following <https://unlicense.org>
+ */
+
+package org.ufoss.kotysa.jdbc
+
+import org.springframework.jdbc.core.JdbcOperations
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.ufoss.kotysa.*
+import java.math.BigDecimal
+
+/**
+ * @sample org.ufoss.kotysa.spring.jdbc.sample.UserRepositorySpringJdbc
+ */
+internal class SqlClientSpringJdbc(
+        private val client: JdbcOperations,
+        override val tables: Tables
+) : SqlClient, DefaultSqlClient {
+
+    /**
+     * Computed property : only created once on first call
+     */
+    private val namedParameterJdbcOperations: NamedParameterJdbcOperations by lazy {
+        NamedParameterJdbcTemplate(client)
+    }
+
+    override fun <T : Any> insert(row: T) {
+        val table = tables.getTable(row::class)
+
+        val parameters = MapSqlParameterSource()
+        table.columns
+                // do nothing for null values with default or Serial type
+                .filterNot { column ->
+                    column.entityGetter(row) == null
+                            && (column.defaultValue != null
+                            || column.isAutoIncrement
+                            || SqlType.SERIAL == column.sqlType
+                            || SqlType.BIGSERIAL == column.sqlType)
+                }
+                .map { column -> tables.getDbValue(column.entityGetter(row)) }
+                .forEachIndexed { index, dbValue -> parameters.addValue("k$index", dbValue)  }
+
+        namedParameterJdbcOperations.update(insertSql(row), parameters)
+    }
+
+    override fun <T : Any> insert(vararg rows: T) {
+        rows.forEach { row -> insert(row) }
+    }
+
+    override fun <T : Any> createTable(table: Table<T>) {
+        createTable(table, false)
+    }
+
+    override fun <T : Any> createTableIfNotExists(table: Table<T>) {
+        createTable(table, true)
+    }
+
+    private fun <T : Any> createTable(table: Table<T>, ifNotExists: Boolean) {
+        val createTableSql = createTableSql(table, ifNotExists)
+        client.execute(createTableSql)
+    }
+
+    override fun <T : Any> deleteFrom(table: Table<T>): SqlClientDeleteOrUpdate.FirstDeleteOrUpdate<T> =
+            SqlClientDeleteSpringJdbc.FirstDelete(namedParameterJdbcOperations, tables, table)
+
+    override fun <T : Any> update(table: Table<T>): SqlClientDeleteOrUpdate.Update<T> =
+            SqlClientUpdateSpringJdbc.FirstUpdate(namedParameterJdbcOperations, tables, table)
+
+    override fun <T : Any, U : Any> select(column: Column<T, U>): SqlClientSelect.FirstSelect<U> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).select(column)
+    override fun <T : Any> select(table: Table<T>): SqlClientSelect.FirstSelect<T> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).select(table)
+    override fun <T : Any> select(dsl: (ValueProvider) -> T): SqlClientSelect.Fromable<T> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).select(dsl)
+    override fun selectCount(): SqlClientSelect.Fromable<Long> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectCount<Any>(null)
+    override fun <T : Any> selectCount(column: Column<*, T>): SqlClientSelect.FirstSelect<Long> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectCount(column)
+    override fun <T : Any, U : Any> selectDistinct(column: Column<T, U>): SqlClientSelect.FirstSelect<U> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectDistinct(column)
+    override fun <T : Any, U : Any> selectMin(column: MinMaxColumn<T, U>): SqlClientSelect.FirstSelect<U> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectMin(column)
+    override fun <T : Any, U : Any> selectMax(column: MinMaxColumn<T, U>): SqlClientSelect.FirstSelect<U> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectMax(column)
+    override fun <T : Any, U : Any> selectAvg(column: NumericColumn<T, U>): SqlClientSelect.FirstSelect<BigDecimal> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectAvg(column)
+    override fun <T : Any> selectSum(column: IntColumn<T>): SqlClientSelect.FirstSelect<Long> =
+            SqlClientSelectSpringJdbc.Selectable(namedParameterJdbcOperations, tables).selectSum(column)
+}
+
+/**
+ * Create a [SqlClient] from a Spring [JdbcOperations] with [Tables] mapping
+ *
+ * @sample org.ufoss.kotysa.spring.jdbc.sample.UserRepositorySpringJdbc
+ */
+public fun JdbcOperations.sqlClient(tables: Tables): SqlClient = SqlClientSpringJdbc(this, tables)
