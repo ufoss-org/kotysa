226dea5785f42a704c86284d5c7c6bbd3b43b194
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-09-17 00:47:44 +0200

    Add kotysa KTor sample

---
+++ samples/kotysa-ktor-jdbc/src/main/kotlin/com/sample/Repositories.kt
@@ -0,0 +1,51 @@
+package com.sample
+
+import org.ufoss.kotysa.SqlClient
+import java.util.*
+
+private val role_user_uuid = UUID.fromString("79e9eb45-2835-49c8-ad3b-c951b591bc7f")
+private val role_admin_uuid = UUID.fromString("67d4306e-d99d-4e54-8b1d-5b1e92691a4e")
+
+class UserRepository(private val client: SqlClient) {
+
+    fun count() = client selectCountAllFrom USER
+
+    fun findAll() = client selectAllFrom USER
+
+    fun findOne(id: Int) =
+            (client selectFrom USER
+                    where USER.id eq id
+                    ).fetchOne()!!
+
+    fun selectWithJoin() =
+            (client.select {
+                UserDto("${it[USER.firstname]} ${it[USER.lastname]}", it[USER.alias], it[ROLE.label]!!)
+            }
+                    from USER innerJoin ROLE on USER.roleId eq ROLE.id
+                    ).fetchAll()
+
+    fun deleteAll() = client deleteAllFrom USER
+
+    fun save(user: User) = client insert user
+
+    fun init() {
+        client createTableIfNotExists USER
+        deleteAll()
+        save(User("John", "Doe", false, role_user_uuid, id = 123))
+        save(User("Big", "Boss", true, role_admin_uuid, "TheBoss"))
+    }
+}
+
+class RoleRepository(private val client: SqlClient) {
+    fun deleteAll() = client deleteAllFrom ROLE
+
+    fun save(role: Role) = client insert role
+
+    fun init() {
+        client createTableIfNotExists ROLE
+        deleteAll()
+        save(Role("user", role_user_uuid))
+        save(Role("admin", role_admin_uuid))
+         save(Role("god"))
+    }
+}
