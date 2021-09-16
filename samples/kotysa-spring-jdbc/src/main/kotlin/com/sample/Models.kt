eee35e0165daf5952d730ee1f78853edfb02daa7
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-09-17 00:46:58 +0200

    cleanup

---
+++ samples/kotysa-spring-jdbc/src/main/kotlin/com/sample/Models.kt
@@ -0,0 +1,49 @@
+package com.sample
+
+import org.ufoss.kotysa.h2.H2Table
+import java.time.LocalDateTime
+import java.util.*
+
+data class Role(
+        val label: String,
+        val id: UUID = UUID.randomUUID()
+)
+
+data class User(
+        val firstname: String,
+        val lastname: String,
+        val isAdmin: Boolean,
+        val roleId: UUID,
+        val alias: String? = null,
+        val creationTime: LocalDateTime = LocalDateTime.now(),
+        val id: Int? = null
+)
+
+/**
+ * Not an entity
+ * name = first_name and last_name
+ */
+data class UserDto(
+        val name: String,
+        val alias: String?,
+        val role: String
+)
+
+// Mapping
+
+object ROLE : H2Table<Role>() {
+    val id = uuid(Role::id)
+            .primaryKey()
+    val label = varchar(Role::label)
+}
+
+object USER : H2Table<User>() {
+    val id = autoIncrementInteger(User::id)
+            .primaryKey()
+    val firstname = varchar(User::firstname)
+    val lastname = varchar(User::lastname)
+    val isAdmin = boolean(User::isAdmin)
+    val roleId = uuid(User::roleId)
+    val alias = varchar(User::alias)
+    val creationTime = dateTime(User::creationTime)
+}
