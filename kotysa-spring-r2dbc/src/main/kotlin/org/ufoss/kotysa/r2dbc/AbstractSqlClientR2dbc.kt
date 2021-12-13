facbdcf68a0bb5fd4c39193a9964ba0c6eca71de
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-12-13 15:25:13 +0100

    try to fix insertAndReturn problem with Spring and annotations

--- kotysa-spring-r2dbc/src/main/kotlin/org/ufoss/kotysa/r2dbc/AbstractSqlClientR2dbc.kt
+++ kotysa-spring-r2dbc/src/main/kotlin/org/ufoss/kotysa/r2dbc/AbstractSqlClientR2dbc.kt
@@ -75,7 +75,8 @@ internal interface AbstractSqlClientR2dbc : DefaultSqlClient {
             }
     }
 
-    private fun <T : Any> fetchLastInserted(row: T, table: KotysaTable<T>): Mono<T> {
+    // fixme 13/12/21 : does not work if set to private
+    public fun <T : Any> fetchLastInserted(row: T, table: KotysaTable<T>): Mono<T> {
         @Suppress("UNCHECKED_CAST")
         val pkColumns = table.primaryKey.columns as List<DbColumn<T, *>>
 
