62ad32c5ab401f2c98ef30cea90f3fd697e57a8a
pull-vert <5107035+pull-vert@users.noreply.github.com>
2021-12-19 19:31:31 +0100

    use real WebClient for tests

--- samples/kotysa-reactive-r2dbc/src/test/kotlin/com/sample/IntegrationTests.kt
+++ samples/kotysa-reactive-r2dbc/src/test/kotlin/com/sample/IntegrationTests.kt
@@ -1,8 +1,9 @@
 package com.sample
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.expectBody
@@ -12,29 +13,29 @@ import org.springframework.test.web.reactive.server.expectBodyList
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class IntegrationTests {
 
-    @Autowired
-    private lateinit var config: WebConfig
-    
-    private val client: WebTestClient by lazy {
-        WebTestClient
-            .bindToRouterFunction(config.appRouter())
+    private lateinit var client: WebTestClient
+
+    @BeforeAll
+    fun before(@LocalServerPort port: Int) {
+        client = WebTestClient.bindToServer()
+            .baseUrl("http://localhost:$port")
             .build()
     }
 
     @Test
     fun `Request HTTP API endpoint for listing all users`() {
         client.get().uri("/api/user").exchange()
-                .expectStatus().is2xxSuccessful
-                .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
-                .expectBodyList<User>()
-                .hasSize(2)
+            .expectStatus().is2xxSuccessful
+            .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
+            .expectBodyList<User>()
+            .hasSize(2)
     }
 
     @Test
     fun `Request HTTP API endpoint for getting one specified user`() {
         client.get().uri("/api/user/123").exchange()
-                .expectStatus().is2xxSuccessful
-                .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
-                .expectBody<User>()
+            .expectStatus().is2xxSuccessful
+            .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
+            .expectBody<User>()
     }
 }
