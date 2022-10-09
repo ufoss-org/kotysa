ce6b2edb05d3142938044d837836983d4f8f910b
pull-vert <5107035+pull-vert@users.noreply.github.com>
2022-10-09 23:19:54 +0200

    update samples for kotysa 2.2.1

--- samples/kotysa-spring-jdbc/src/main/kotlin/com/sample/DataConfig.kt
+++ samples/kotysa-spring-jdbc/src/main/kotlin/com/sample/DataConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration
 import org.springframework.jdbc.core.JdbcOperations
-import org.springframework.transaction.support.TransactionOperations
+import org.springframework.transaction.support.TransactionTemplate
 import org.ufoss.kotysa.spring.jdbc.sqlClient
 import org.ufoss.kotysa.spring.jdbc.transaction.transactionalOp
 import org.ufoss.kotysa.tables
@@ -19,5 +19,5 @@ class DataConfig : AbstractJdbcConfiguration() {
     fun sqlClient(dbClient: JdbcOperations) = dbClient.sqlClient(h2Tables)
 
     @Bean
-    fun operator(op: TransactionOperations) = op.transactionalOp()
+    fun operator(op: TransactionTemplate) = op.transactionalOp()
 }
