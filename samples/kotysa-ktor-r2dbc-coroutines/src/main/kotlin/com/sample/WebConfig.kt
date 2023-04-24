3fc9c4bf0f49bcd352f266078f146371a37af104
pull-vert <5107035+pull-vert@users.noreply.github.com>
2023-04-24 23:21:20 +0200

    Bump to Ktor 2.3.0 that supports Flow<T> serialization

--- samples/kotysa-ktor-r2dbc-coroutines/src/main/kotlin/com/sample/WebConfig.kt
+++ samples/kotysa-ktor-r2dbc-coroutines/src/main/kotlin/com/sample/WebConfig.kt
@@ -1,20 +1,12 @@
 package com.sample
 
-import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.utils.io.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collectIndexed
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToStream
 import org.kodein.di.instance
 import org.kodein.di.ktor.closestDI
-import java.io.OutputStream
 
 fun Application.webConfig() {
     install(ContentNegotiation) {
@@ -23,30 +15,13 @@ fun Application.webConfig() {
 
     routing {
         val userRepository by closestDI().instance<UserRepository>()
-        val json by closestDI().instance<Json>()
 
         get("/api/users") {
-            call.respondOutputStream(ContentType.Application.Json) {
-                userRepository.findAll().writeToStream(this, json)
-            }
+            call.respond(userRepository.findAll())
         }
 
         get("/api/users/{id}") {
             call.respond(userRepository.findOne(call.parameters["id"]!!.toInt()))
         }
     }
-}
-
-// inspiration : https://github.com/Kotlin/kotlinx.serialization/blob/master/formats/json-tests/jvmTest/src/kotlinx/serialization/features/JsonLazySequenceTest.kt
-@OptIn(ExperimentalSerializationApi::class)
-@Suppress("BlockingMethodInNonBlockingContext") // ktor already makes sure this executes in Dispatchers.IO context
-private suspend inline fun <reified T> Flow<T>.writeToStream(os: OutputStream, json: Json) {
-    os.write("[".encodeToByteArray())
-    collectIndexed { index, value ->
-        if (index > 0) {
-            os.write(",".encodeToByteArray())
-        }
-        json.encodeToStream(value, os)
-    }
-    os.write("]".encodeToByteArray())
 }
