ccd62556fbcc444366abc8bc35513d0b3b791f70
pull-vert <5107035+pull-vert@users.noreply.github.com>
2023-09-11 11:10:59 +0200

    pre-3.2.1 release adjustment

--- kotysa-spring-r2dbc/src/jvmMain/java/module-info.java
+++ kotysa-spring-r2dbc/src/jvmMain/java/module-info.java
@@ -7,8 +7,9 @@ module kotysa.spring.r2dbc {
     requires kotysa.core;
     requires r2dbc.spi;
     requires spring.r2dbc;
-    
-    requires static kotlinx.coroutines.core;
+
+    // todo uncomment with kotlinx.coroutines 1.7.x
+//    requires static kotlinx.coroutines.core;
 
     exports org.ufoss.kotysa.spring.r2dbc;
     exports org.ufoss.kotysa.spring.r2dbc.transaction;
